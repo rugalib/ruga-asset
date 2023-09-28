a86b324a9a5ce6a237012f4e934a764b72947519
Roland Rusch <roland@rusch.lu>
2023-09-28 08:30:47 +0200

    #16 Change License to AGPL-3.0-only

--- src/Helper/HeadLinkAssets.php
+++ src/Helper/HeadLinkAssets.php
@@ -1,6 +1,11 @@
 <?php
+/*
+ * SPDX-FileCopyrightText: 2023 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 declare(strict_types=1);
+
 
 namespace Ruga\Asset\Helper;
 
